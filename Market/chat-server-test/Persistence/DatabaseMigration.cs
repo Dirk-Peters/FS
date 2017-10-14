f4ce6fe5859359632eb395f826cd8e969bf3d9b7
Dirk Peters <dirk.peters.ath@gmx.de>
2017-10-14 21:28:55 +0200

    learn usage of dapper with sqlite

--- Market/chat-server-test/Persistence/DatabaseMigration.cs
+++ Market/chat-server-test/Persistence/DatabaseMigration.cs
@@ -1,6 +1,9 @@
-﻿using chat_server.Persistence;
+﻿using System;
+using System.Data;
+using chat_server.Persistence;
 using chat_server.Persistence.Migrations;
-using FluentMigrator;
+using Dapper;
+using FluentAssertions;
 using FluentMigrator.Runner;
 using FluentMigrator.Runner.Announcers;
 using FluentMigrator.Runner.Generators.SQLite;
@@ -15,21 +18,40 @@ namespace chat_server_test.Persistence
     [TestFixture]
     public static class DatabaseMigration
     {
-        [Test]
-        public static void Can_execute_fluent_migrator_with_current_migrations()
-        {
-            using (var dbFile = new TempFile("sqlite"))
-            {
-                var connection = new SqliteConnection($"Data Source={dbFile.Path};");
-                IRunnerContext context = new RunnerContext(new ConsoleAnnouncer());
-                IMigrationProcessor processor = new SQLiteProcessor(
+        private static MigrationRunner MigrationRunner(IDbConnection connection) =>
+            new MigrationRunner(
+                typeof(InitialSchema).Assembly,
+                new RunnerContext(new ConsoleAnnouncer()),
+                new SQLiteProcessor(
                     connection,
                     new SQLiteGenerator(),
                     new ConsoleAnnouncer(),
                     new ProcessorOptions(),
-                    new SqliteCoreDbFactory());
-                var runner = new MigrationRunner(typeof(InitialSchema).Assembly, context, processor);
-                Assert.DoesNotThrow(() => runner.MigrateUp());
+                    new SqliteCoreDbFactory()));
+
+        private static SqliteConnection SqLiteFileConnection(TempFile dbFile) =>
+            new SqliteConnection($"Data Source={dbFile.Path};");
+
+        [Test]
+        public static void Can_execute_fluent_migrator_with_current_migrations()
+        {
+            using (var dbFile = new TempFile("sqlite"))
+            using (var connection = SqLiteFileConnection(dbFile))
+            {
+                Assert.DoesNotThrow(() => MigrationRunner(connection).MigrateUp());
+            }
+        }
+
+        [Test]
+        public static void Can_insert_rows_with_dapper()
+        {
+            using (var dbFile = new TempFile("sqlite"))
+            using (var connection = SqLiteFileConnection(dbFile))
+            {
+                MigrationRunner(connection).MigrateUp();
+                connection.Execute(@"insert into Sessions(Id,Name) values (@Id,@Name)",
+                        new {Id = Guid.NewGuid(), Name = "test"})
+                    .Should().Be(1);
             }
         }
     }
